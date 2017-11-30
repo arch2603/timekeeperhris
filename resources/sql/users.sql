3e036fd3134853b4432359e565863d243a7e6ff2
arch2603 <archiesua@gmail.com>
2017-11-30 12:56:22 +1000

    add sql file

---
+++ resources/sql/users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (user_name, name, email, password, remember_token)
+VALUES
+('arch2603', 'Archie', 'archie@gmail.com', 'arcsax09', null);
+
+INSERT INTO users (user_name, name, email, password, remember_token)
+VALUES
+('hm123', 'Henry', 'henry.mailo@gmail.com', 'arcsax09', null);
